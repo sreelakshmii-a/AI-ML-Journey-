11617d2718304b974cb5c1983fe41d98b8c78170
sreelakshmii-a <sreelakshmia1723@gmail.com>
2026-02-06 01:49:14 +0530

    Learningn Phase

--- Python Fudamentals/Projects/Drink_Water/main.py
+++ Python Fudamentals/Projects/Drink_Water/main.py
@@ -1,2 +1,6 @@
 import time
-
+from plyer import notification
+while True:
+  notification.notify(title="DeHydration Alert",
+                      message="You need to drink some water")
+  time.sleep(5)
